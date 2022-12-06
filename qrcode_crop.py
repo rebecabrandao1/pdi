96f5a84d3ec4363238c1387ffe5327d9d11b0ae4
Rebeca Brandão <58791853+rebecabrandao1@users.noreply.github.com>
2022-12-06 13:59:18 -0300

    Add files via upload

---
+++ qrcode_crop.py
@@ -0,0 +1,108 @@
+#%%
+import numpy as np
+import cv2 as cv
+from pyzbar.pyzbar import decode
+import matplotlib.pyplot as plt
+#%%
+# def verificaqr(image):
+
+
+#    scale = 0.3
+#    width = int(image.shape[1] * scale)
+#    height = int(image.shape[0] * scale)
+#    image = cv.resize(image, (width, height))
+# #achando um limiar
+#    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+#    _, thresh = cv.threshold(gray, 120, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+# #%%
+# # The bigger the kernel, the more the white region increases.
+# # If the resizing step was ignored, then the kernel will have to be bigger
+# # than the one given here.
+#    kernel = np.ones((3, 3), np.uint8)
+#    thresh = cv.dilate(thresh, kernel, iterations=1)
+#    contours, _ = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+# #%%
+#    bboxes = []
+#    for cnt in contours:
+#       area = cv.contourArea(cnt)
+#       xmin, ymin, width, height = cv.boundingRect(cnt)
+#       extent = area / (width * height)
+  
+#   # filter non-rectangular objects and small objects
+#       if (extent > np.pi / 4) and (area > 100):
+#        bboxes.append((xmin, ymin, xmin + width, ymin + height))#%%
+# qrs = []
+# info = set()
+# for xmin, ymin, xmax, ymax in bboxes:
+#   roi = image[ymin:ymax, xmin:xmax]
+#   detections = pyzbar.decode(roi, symbols=[pyzbar.ZBarSymbol.QRCODE])
+#   for barcode in detections:
+#      info.add(barcode.data)
+#      # bounding box coordinates
+#      x, y, w, h = barcode.rect
+#      qrs.append((xmin + x, ymin + y, xmin + x + w, ymin + y + height))
+ #%%
+
+img = cv.imread('sophiaeosmeninos/CERTA.jpg', cv.IMREAD_GRAYSCALE)
+_, thresh = cv.threshold(img, 120, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+plt.imshow(thresh, cmap = 'gray')
+#%%
+#window_name = 'Beontag'
+
+x1 = 0 #0
+y1 = 400 #850
+x2 = 1200 #2000 #1080
+y2 = 750 #1300
+img =  cv.rotate(thresh, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+
+frame = img[y1:y2, x1:x2]
+out = frame.copy()
+plt.imshow(out, cmap = 'gray')
+#%%
+kernel = np.ones((3, 3), np.uint8)
+thresh = cv.dilate(out, kernel, iterations=1)
+contours, _ = cv.findContours(out, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+#%%
+bboxes = []
+for cnt in contours:
+  area = cv.contourArea(cnt)
+  xmin, ymin, width, height = cv.boundingRect(cnt)
+  extent = area / (width * height)
+  #print('entra aqui')
+  if (extent > np.pi / 4) and (area > 100):
+    bboxes.append((xmin, ymin, xmin + width, ymin + height))#%%
+qrs = []
+info = set()
+for xmin, ymin, xmax, ymax in bboxes:
+  roi = out[ymin:ymax, xmin:xmax]
+  detections = decode(roi)
+  for barcode in detections:
+     info.add(barcode.data.decode('utf-8'))
+     # bounding box coordinates
+     x, y, w, h = barcode.rect
+     qrs.append((xmin + x, ymin + y, xmin + x + w, ymin + y + height))
+#print('informações do qr')
+print(info)
+
+#%%
+#for d in decode(out):
+    #print('entrou')
+    #s = d.data.decode()#qr code reader result --> comparar no comparador do roger
+    #print(s) #compare with seriesnumber
+    #print('devia estar aqui')
+    #out= cv.rectangle(out, (d.rect.left, d.rect.top),
+    #                            (d.rect.left + d.rect.width, d.rect.top + d.rect.height), (0, 255, 0), 3)
+    #out = cv.putText(out, s, (d.rect.left, d.rect.top + d.rect.height),
+     #                       cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv.LINE_AA)
+#cv.imshow(window_name, frame)
+# Display the resulting frame
+#%%
+
+#plt.imshow(out)
+
+
+
+
+# %%
